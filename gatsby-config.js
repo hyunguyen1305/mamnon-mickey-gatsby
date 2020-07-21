370e38868891d859581ae2dea1393e00ef650527
hyunguyen1305 <huy.nguyenngoc1305@gmail.com>
2020-07-22 00:27:23 +0700

    add key enviroment on Package1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 const activeEnv =
   process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
 console.log(`Using environment config: '${activeEnv}'`)
+console.log(`Key: '${process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN}'`)
 require("dotenv").config({
   path: `.env.${activeEnv}`,
 })
